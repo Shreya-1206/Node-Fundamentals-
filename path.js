9985741ead8ec0d4276a74b9bddaec43a9e015a9
Shreya <shreyaray.1206@gmail.com>
2024-05-08 17:09:51 +0530

    node fundamentals

--- path.js
+++ path.js
@@ -1,5 +1,8 @@
 import path from 'path';
 
 var file = path.format({dir :"c:\\Users\\DELL\\Desktop\\JavascriptPrac", base : "hello.txt"});
-console.log(path.parse(file));
+console.log(file);
 console.log(path.parse("c:\\Users\\DELL\\Desktop\\JavascriptPrac\\hello.text"));
+
+
+//parsing through paths analyze it with a detail
